d0460bd340680cd3948fe95f179d1d2a037e261c
tobias <tobias.omondi@student.moringaschool.com>
2024-10-07 18:07:58 +0300

    react setup

---
+++ studio/src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Contact from './components/Contact';
+import Gallery from './components/Gallery';
+import Podcast from './components/Podcast';
+import Home from './components/Home';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/' element = {<Home />}/>
+          <Route path='/gallery' element = {<Gallery/>}/>
+          <Route path='/podcast' element = {<Podcast/> }/>
+          <Route path='/contact' element = {<Contact/> }/>
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ studio/src/components/Podcast.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Podcast = () => {
+  return (
+    <div>
+      <h2>Podcast page </h2>
+    </div>
+  )
+}
+
+export default Podcast
