70bd002e171f9426e5deca62513767cec3291880
tobias <tobias.omondi@student.moringaschool.com>
2024-10-07 18:55:44 +0300

    react setup

--- studio/src/components/Navbar.js
+++ studio/src/components/Navbar.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import {Link} from "react-router-dom";
 import logo from "../studioAsset/logo.png"
+import './Navbar.css'
 
 const Navbar = () => {
   return (
-    <div>
+    <div className = 'main_container'>
       <div className='Logo'>
         <img src={logo} alt='Logo' />
         <nav>
@@ -12,9 +13,9 @@ const Navbar = () => {
             <li><Link to='/' className='nav_link'>Home</Link></li>
             <li><Link to='/gallery' className='nav_link'>Gallery</Link></li>
             <li><Link to='/podcast' className='nav_link'>Podcast</Link></li>
-            <li><Link to='/contact' className='nav_link'>Contact</Link></li>
         </ul>
         </nav>
+        <button type = 'submit'><Link to = '/contact' className = 'btn_link'> Contact us </Link> </button>
       </div>
     </div>
   )
