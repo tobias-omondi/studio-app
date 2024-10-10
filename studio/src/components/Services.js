c830924dbf1aeabd10fc64ac2bfd935ec9f4d710
tobias <tobias.omondi@student.moringaschool.com>
2024-10-10 22:13:53 +0300

    added react swiper

--- studio/src/components/Services.js
+++ studio/src/components/Services.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import './Services.css';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Navigation, Pagination, Autoplay } from 'swiper/modules'; // Import necessary modules
-import 'swiper/swiper-bundle.css'; // Import Swiper styles
-import Services1 from "../studioAsset/services7.jpeg";
-import Services2 from "../studioAsset/services1.jpg";
-import Services3 from "../studioAsset/services3.jpeg";
-import Services4 from "../studioAsset/services4.jpeg";
-import Services5 from "../studioAsset/services2.jpeg";
+import { Navigation, Pagination, Autoplay } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import Services1 from "../studioAsset/services10.jpeg";
+import Services2 from "../studioAsset/services16.jpg";
+import Services3 from "../studioAsset/services15.jpeg";
+import Services4 from "../studioAsset/services15.jpg";
+import Services5 from "../studioAsset/services14.jpg";
 import Services6 from "../studioAsset/services6.jpeg";
 import Services7 from "../studioAsset/services8.jpeg";
-import Services8 from "../studioAsset/services9.jpeg";
 
 // Array of service images and descriptions
 const imageServices = [
@@ -21,7 +22,6 @@ const imageServices = [
     { src: Services5, description: "Description for Service 5" },
     { src: Services6, description: "Description for Service 6" },
     { src: Services7, description: "Description for Service 7" },
-    { src: Services8, description: "Description for Service 8" },
 ];
 
 const Services = () => {
@@ -33,27 +33,23 @@ const Services = () => {
       <Swiper
         modules={[Navigation, Pagination, Autoplay]} // Register modules
         spaceBetween={30} // Space between slides
-        autoplay={{ delay: 2500, disableOnInteraction: false }} // Autoplay settings
+        autoplay={{ delay: 3000, disableOnInteraction: false }} // Autoplay settings
         pagination={{ clickable: true }} // Pagination bullets
         navigation={true} // Navigation arrows enabled
         breakpoints={{
-          // Responsive breakpoints
-          640: {
-            slidesPerView: 1, // Show 1 slide on mobile screens
-          },
-          768: {
-            slidesPerView: 2, // Show 2 slides on tablet screens
-          },
-          1024: {
-            slidesPerView: 3, // Show 3 slides on desktop screens
-          }
+          640: { slidesPerView: 1 }, // Show 1 slide on mobile screens
+          768: { slidesPerView: 2 }, // Show 2 slides on tablet screens
+          1024: { slidesPerView: 3 }, // Show 3 slides on desktop screens
         }}
       >
         {imageServices.map((service, index) => (
           <SwiperSlide key={index} className='services_slide'>
-            <img src={service.src} alt={`Service ${index + 1}`} style={{ width: '100%'}} className='image_slider'/>
-            <div className='caption'>
-              <p>{service.description}</p>
+            {/* Container to wrap the image and the overlay text */}
+            <div className='image_container'>
+              <img src={service.src} alt={`Service ${index + 1}`} className='image_slider' />
+              <div className='caption_description'>
+                <p>{service.description}</p>
+              </div>
             </div>
           </SwiperSlide>
         ))}
