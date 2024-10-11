3a9fe1162c5c0564939c9cedeac336b969dcd529
tobias <tobias.omondi@student.moringaschool.com>
2024-10-11 22:38:45 +0300

    added componets

---
+++ studio/src/components/Testimonials.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Social from './Social';
+
+const Testimonials = () => {
+  return (
+    <>
+    <div>
+      <h2 style={{color:'white'}}>Testimonials</h2>
+    </div>
+    <Social />
+    </>
+  )
+}
+
+export default Testimonials;
