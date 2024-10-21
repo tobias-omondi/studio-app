31672ebe2279f16243dc53c87b1a211936ae9434
tobias <tobias.omondi@student.moringaschool.com>
2024-10-21 16:02:02 +0300

    added components

---
+++ studio/src/components/Message.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./Message.css"
+import Footer from './Footer'
+
+const Message = () => {
+  return (
+    <div>
+    <div>
+      <h2>Message Emba</h2>
+    </div>
+    <Footer />
+    </div>
+  )
+}
+
+export default Message
