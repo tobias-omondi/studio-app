eea88c030fc66c47ac92d55580ec0a7ff03d3585
tobias <tobias.omondi@student.moringaschool.com>
2024-10-22 15:52:38 +0300

    added footer

--- studio/src/components/Footer.js
+++ studio/src/components/Footer.js
@@ -1,13 +1,41 @@
 import React from 'react'
 import './Footer.css'
+import logo from "../studioAsset/logo.png";
+import { ImYoutube } from "react-icons/im";
+import { FaTiktok , FaInstagram, FaTwitter} from "react-icons/fa";
 
 const Footer = () => {
+
+  const currentYear = new Date ().getFullYear();
   return (
-    <>
-    <div className='Footer_container'>
-      <h2 style={{color:'black'}}>Footer</h2>
+    < div className='Footer-container'>
+    <div className='Footer-menu'>
+      <h2 style={{color:'black'}}>EMBA ONE STUDIO </h2>
+      <p>Connecting creativity with innovation to bring your ideas to life.</p>
+      <div className='Footer-Logo'>
+        <img src={logo} alt='Logo' />
+      </div>
+      <div className='socialmedia-logo'>
+        <ul>
+          <li>
+            <a><ImYoutube /></a>
+          </li>
+          <li>
+            <a><FaInstagram /></a>
+          </li>
+          <li>
+            <a><FaTwitter /></a>
+          </li>
+          <li>
+            <a><FaTiktok /></a>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <p>&copy; {currentYear} EMBA ONE STUDIO. All rights reserved.</p>
+      </div>
     </div>
-    </>
+    </div>
   )
 }
 
