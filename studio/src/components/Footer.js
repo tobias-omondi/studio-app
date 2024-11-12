663e7627b56f7a0a534237ba4500a40622a8edc2
tobias <tobias.omondi@student.moringaschool.com>
2024-11-12 16:00:39 +0300

    swiper testiomonial

--- studio/src/components/Footer.js
+++ studio/src/components/Footer.js
@@ -9,12 +9,16 @@ const Footer = () => {
   const currentYear = new Date ().getFullYear();
   return (
     < div className='Footer-container'>
+
+
     <div className='Footer-menu'>
       <h2 style={{color:'black'}}>EMBA ONE STUDIO </h2>
       <p>Connecting creativity with innovation to bring your ideas to life.</p>
       <div className='Footer-Logo'>
         <img src={logo} alt='Logo' />
       </div>
+
+
       <div className='socialmedia-logo'>
         <ul>
           <li>
@@ -31,11 +35,13 @@ const Footer = () => {
           </li>
         </ul>
       </div>
-      <div>
-        <p>&copy; {currentYear} EMBA ONE STUDIO. All rights reserved.</p>
+
+      <div className='copywrite-container'>
+        <p>&copy; <i>{currentYear} EMBA ONE STUDIO.</i> All rights reserved.</p>
       </div>
     </div>
-    <div>
+
+    <div className='social-link-footer'>
       <ul>
         <li>
           <a href='/'>Home</a>
