57db4af9f6ac2a1f8a8d8a5843fd6e747e474ab5
tobias <tobias.omondi@student.moringaschool.com>
2024-11-13 16:39:12 +0300

    fixed landing page

--- studio/src/components/Footer.js
+++ studio/src/components/Footer.js
@@ -1,66 +1,59 @@
 import React from 'react'
-import './Footer.css'
-import logo from "../studioAsset/logo.png";
+import { FaTiktok , FaInstagram} from "react-icons/fa";
+import { FaSquareXTwitter } from "react-icons/fa6";
 import { ImYoutube } from "react-icons/im";
-import { FaTiktok , FaInstagram, FaTwitter} from "react-icons/fa";
+import "./Footer.css"
+import logofooter from '../studioAsset/logo.png'
 
 const Footer = () => {
 
-  const currentYear = new Date ().getFullYear();
+  const currentYear = new Date ().getFullYear()
   return (
-    < div className='Footer-container'>
+    <>
+    <div className='footer-container'>
+      <div className='logo-footer'>
+        <img src={logofooter} alt='logo' />
 
+        <div className='logo-heading'>
+          <h2>Emba One Studio</h2>
+        </div>
 
-    <div className='Footer-menu'>
-      <h2 style={{color:'black'}}>EMBA ONE STUDIO </h2>
-      <p>Connecting creativity with innovation to bring your ideas to life.</p>
-      <div className='Footer-Logo'>
-        <img src={logo} alt='Logo' />
       </div>
-
-
-      <div className='socialmedia-logo'>
+      <div className='footer-icons'>
+          <ul>
+            <li>
+              <a href=''> <FaSquareXTwitter /></a>
+            </li>
+            <li>
+              <a href=''> <FaInstagram /></a>
+            </li>
+            <li>
+              <a href=''> <FaTiktok /></a>
+            </li>
+            <li>
+              <a href=''> <ImYoutube /></a>
+            </li>
+          </ul>
+        </div>
+        
+      <div className='social-links'>
+        <h2>OUR WEB LINKS</h2>
         <ul>
           <li>
-            <a><ImYoutube /></a>
+            <a href='#'>Services</a>
           </li>
           <li>
-            <a><FaInstagram /></a>
+            <a href='#'>Gallery</a>
           </li>
           <li>
-            <a><FaTwitter /></a>
-          </li>
-          <li>
-            <a><FaTiktok /></a>
+            <a href='#'>About us</a>
           </li>
         </ul>
-      </div>
-
-      <div className='copywrite-container'>
-        <p>&copy; <i>{currentYear} EMBA ONE STUDIO.</i> All rights reserved.</p>
+        <p>Got questions, ideas, or just want to say hello?  Stay in touch and let’s keep this conversation going!</p>
       </div>
     </div>
-
-    <div className='social-link-footer'>
-      <ul>
-        <li>
-          <a href='/'>Home</a>
-        </li>
-        <li>
-          <a href='/gallery'>GALLERY</a>
-        </li>
-        <li>
-          <a href='/podcast'>PODCAST</a>
-        </li>
-        <li>
-          <a href='/videos'>VIDEOS</a>
-        </li>
-        <li>
-          <a href='/contact'>CONTACT</a>
-        </li>
-      </ul>
-    </div>
-    </div>
+    <p style={{color:'#8b8b8b', textAlign:'center', fontFamily:'"Afacad Flux", sans-serif', backgroundColor:'#000',textDecoration:'overline'}}>© {currentYear} Emba One Studio. All rights reserved.</p>
+    </>
   )
 }
 
