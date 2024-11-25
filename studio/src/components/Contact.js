18beba2f84f1bae23117e77760c52f55209fb688
tobias <tobias.omondi@student.moringaschool.com>
2024-11-25 12:31:21 +0300

    added gallery

--- studio/src/components/Contact.js
+++ studio/src/components/Contact.js
@@ -1,11 +1,105 @@
-import React from 'react'
+import React, { useState } from 'react';
+import "./Message.css";
+import Footer from './Footer';
+import {motion} from "framer-motion";
+
+const messageCaption = [
+    {
+        messageHeading: "EMBA ONE STUDIO"
+    },
+    {
+        message: "We're so glad you found us! Whether you're here to explore our services, check out what’s new, or simply curious about what we do, we’re excited to connect with you. 💬 Got questions, ideas, or just want to say hello? We'd love to hear from you! We’re always happy to share more about what we offer and how we can help. Stay in touch and let’s keep this conversation going!"
+    },
+];
 
 const Contact = () => {
-  return (
-    <div>
-      <h2> Contact page</h2>
-    </div>
-  )
+    // State to hold form inputs
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        phoneNumber: '',
+        message: '',
+    });
+
+    // Handle input changes
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({ ...formData, [name]: value });
+    };
+
+    // Handle form submission
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        // Implement your form submission logic here (e.g., sending data to an API)
+        console.log("Form submitted:", formData);
+        // Optionally, reset the form after submission
+        setFormData({ name: '', email: '', phoneNumber: '', message: '' });
+    };
+
+    return (
+        <>
+        <div className="message-container">
+            <div className="message-heading">
+                {messageCaption.map((item, index) => (
+                    <div key={index} className="message-item">
+                        {item.messageHeading && <h2 className="message-heading">{item.messageHeading}</h2>}
+                        {item.message && <p className="message-text">{item.message}</p>}
+                    </div>
+                ))}
+            </div>
+            <div className='message-inputs'>
+                <form onSubmit={handleSubmit}>
+                    <input
+                        type='text'
+                        id='name'
+                        name='name'
+                        placeholder='Name:'
+                        value={formData.name}
+                        onChange={handleChange}
+                        required
+                    />
+                    <input
+                        type='tel'
+                        id='phoneNumber'
+                        name='phoneNumber'
+                        placeholder='Phone Number:'
+                        value={formData.phoneNumber}
+                        onChange={handleChange}
+                        required
+                    />
+                    <input
+                        type='email'
+                        id='email'
+                        name='email'
+                        placeholder='Email:'
+                        value={formData.email}
+                        onChange={handleChange}
+                        required
+                    />
+                    <textarea
+                        id='message'
+                        name='message'
+                        placeholder='Message:'
+                        value={formData.message}
+                        onChange={handleChange}
+                        required
+                    ></textarea>
+                    <motion.button
+                        type='submit' className='btn-message'
+                        initial={{ opacity: 0, scale: 0.8 }}
+                        animate={{ opacity: 1, scale: 1 }}
+                        transition={{ duration: 0.5 }}
+                        whileHover={{ scale: 1.05 }}
+                        whileTap={{ scale: 0.95 }}
+                    >
+                        Send Message
+                    </motion.button>
+                </form>
+            </div>
+        </div>
+        <Footer />
+        </>
+    );
 }
 
-export default Contact
+export default Contact;
--- studio/src/components/GalleryImages.js
+++ studio/src/components/GalleryImages.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import "./Gallery.css";
 import Footer from "./Footer";
+import AOS from 'aos';
+import 'aos/dist/aos.css'; 
 
 const GalleryImages = () => {
   const [images, setImages] = useState([]);
@@ -8,30 +10,35 @@ const GalleryImages = () => {
   const [error, setError] = useState(null);
 
   const fetchData = async () => {
-  try {
-    const response = await fetch(
-      "https://gallery-backend-for-studio-3.onrender.com/images"
-    );
-    if (!response.ok) {
-      throw new Error("Failed to fetch gallery data");
+    try {
+      const response = await fetch(
+        "https://gallery-backend-for-studio-3.onrender.com/images"
+      );
+      if (!response.ok) {
+        throw new Error("Failed to fetch gallery data");
+      }
+      const data = await response.json();
+
+      setTimeout(() => {
+        setImages(data);
+        setLoading(false); 
+      }, 3000); 
+    } catch (err) {
+      setError(err.message);
+      setLoading(false);
     }
-    const data = await response.json();
+  };
 
-    // Simulate slow response
-    setTimeout(() => {
-      setImages(data);
-      setLoading(false); // Ensure this is delayed
-    }, 3000); // 3-second delay
-  } catch (err) {
-    setError(err.message);
-    setLoading(false);
-  }
-};
   console.log("Loading:", loading);
-
 
   useEffect(() => {
     fetchData();
+    
+    AOS.init({
+      duration: 1000, 
+      easing: 'ease-in-out', 
+      once: true, 
+    });
   }, []);
 
   if (loading) {
@@ -48,25 +55,32 @@ const GalleryImages = () => {
 
   return (
     <>
-    <div className="Gallery-background-color">
-      <div className="Gallery-thumb">
-        <h2>
-          <span className="thumb_letter">O</span>ur Images
-        </h2>
+      <div className="Gallery-background-color">
+        <div className="Gallery-thumb">
+          <h2>
+            <span className="thumb_letter">O</span>ur Images
+          </h2>
+        </div>
+        <div className="gallery-album">
+          {images.map((image, index) => (
+            <div
+              key={index}
+              className="gallery-item"
+              data-aos="fade-zoom-in"
+              data-aos-easing="ease-in-back"
+              data-aos-delay="100"
+              data-aos-offset="0"
+            >
+              <img
+                src={image.image_url}
+                alt={image.title}
+                className="gallery-image"
+              />
+            </div>
+          ))}
+        </div>
       </div>
-      <div className="gallery-album">
-        {images.map((image, index) => (
-          <div key={index} className="gallery-item">
-            <img
-              src={image.image_url}
-              alt={image.title}
-              className="gallery-image"
-            />
-          </div>
-        ))}
-      </div>
-    </div>
-    <Footer />
+      <Footer />
     </>
   );
 };
